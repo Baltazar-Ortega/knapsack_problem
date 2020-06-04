c9ed53a1604de1d2fbd977dc1c03f0e93609e765
Baltazar-Ortega} <kalbertog121@gmail.com>
2020-06-03 21:56:03 -0500

    input, proceso, resultado (todas las variables)

--- solver.py
+++ solver.py
@@ -11,60 +11,13 @@ import re
 
 arbol = Tree()
 mejor_nodo = {}
-archivo = open("data2.txt", "r") #Abrir el archivo para leer
-num_format = re.compile("^[\-]?[1-9]") #Expresión regular para encontrar números enteros.
-lines = [] #Lista para guardar renglones de archivo
 
-def resultado():
-    o=0
-    print(arbol.show())
+def input():
+    archivo = open("input.txt", "r") #Abrir el archivo para leer
+    num_format = re.compile("^[\-]?[1-9]") #Expresión regular para encontrar números enteros.
+    lines = [] #Lista para guardar renglones de archivo
 
-    nodos_ordenados_indices = [0] # el original es 0
-    nodos_ordenados = []
-
-    for nombre in list(arbol.expand_tree(mode=Tree.DEPTH))[1:]:
-         nodos_ordenados_indices.append(int(nombre[4]))
-
-
-    for i in nodos_ordenados_indices:
-
-        for j, nodo in enumerate(arbol.all_nodes()):
-            if i == j:
-                nodos_ordenados.append(nodo)
-    
-    # print("Indices  ordenados: ", nodos_ordenados_indices)
-
-    for i, nodo in enumerate(nodos_ordenados):
-        if i == 0:
-            print("\n\n~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~")
-            print("\n\n\t Problema original \n\n")
-            print("~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~")
-        else:
-            print("\n\n~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~")
-            print("\n\n\t Subproblema ", i, " \n\n")
-            print("~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~")
-        print(nodo)
-        print("~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~")
-
-    for o,nodo in enumerate(nodos_ordenados):
-        if nodo.data['resultado']['obj'] == mejor_nodo['z']:
-            num=o
-    # print("Nodos ordenados: ", nodos_ordenados)
-
-    print("\n\n----------------------------------")
-    print("\n\n\t Solucion óptima: \n")
-    print("\n\t Subproblema ", num,)
-    print("----------------------------------")
-    print("\n z* = ", int(mejor_nodo['z']))
-    print("\n x1 = ", int(mejor_nodo['resultado']['v_x1']))
-    print("\n x1 = ", int(mejor_nodo['resultado']['v_x2']))
-    print("----------------------------------")    
-
-def main():
-    tipo = 'LpMaximize'
-    numbers = [] #Lista de coeficientes y costos
-
-    problema = LpProblem("Ej2_Branch_and_Bound", LpMaximize)
+    numeros = [] #Lista de coeficientes y costos
     
     #Leer por renglon en el archivo
     for line in archivo:
@@ -80,15 +33,15 @@ def main():
             isnumber = re.match(num_format, str_aux) #Devuelve un booleano si el número cumple con la expresión regular
 
             if isnumber: #Si la cadena es un número ENTERO, lo guarda
-                numbers.append(str_aux)
+                numeros.append(str_aux)
 
-    numbers = list(map(int, numbers)) #Transforma lista de caracteres a int
+    numeros = list(map(int, numeros)) #Transforma lista de caracteres a int
 
-    aux_coe = numbers[0] #Toma el valor para saber cuantas variables tiene el problema
+    aux_coe = numeros[0] #Toma el valor para saber cuantas variables tiene el problema
     
     i = 1 #Contador
     coeficientes = {}
-    for s in numbers[1 : aux_coe + 1]: #For para guardar los coeficientes
+    for s in numeros[1 : aux_coe + 1]: #For para guardar los coeficientes
         x = 'x'
         aux = str(i)
         x = 'x' + aux
@@ -96,13 +49,13 @@ def main():
         coeficientes.update({x : s})
 
     i = 1 #Contador
-    costosRes1 = {}
-    for s in numbers[aux_coe + 1 : -1]: #For para guardar las restricciones
+    pesos = {}
+    for s in numeros[aux_coe + 1 : -1]: #For para guardar las restricciones
         x = 'x'
         aux = str(i)
         x = 'x' + aux
         i = i + 1
-        costosRes1.update({x : s})
+        pesos.update({x : s})
 
     xs = []
     for i in range(1, aux_coe + 1): #For para guardar las xs
@@ -111,29 +64,28 @@ def main():
         x = 'x' + aux
         xs.append(x)
 
+    return numeros, coeficientes, pesos, xs
+
+
+
+
+def proceso():
+
+    problema = LpProblem("PIA", LpMaximize)
+
+    numeros, coeficientes, pesos, xs = input()
+
     x_vars = LpVariable.dicts("v", xs, 0) # v_x1, v_x2
-    print("x_vars: ", x_vars)
+    
 
     problema += lpSum([coeficientes[i] * x_vars[i] for i in xs]), 'obj' # funcion objetivo
 
-    problema += lpSum([costosRes1[i] * x_vars[i] for i in xs]) <= numbers[-1]
+    problema += lpSum([pesos[i] * x_vars[i] for i in xs]) <= numeros[-1]
 
     problema.solve()
 
-    # print(problema)
-
-    # for variable in problema.variables():
-    #         print(variable.name, "=", variable.varValue)
-
-    # print(value(problema.objective))
-
-    # print("Estatus: ", LpStatus[problema.status])
-
-    # if LpStatus[problema.status] != 'Optimal':
-    #     print("No se podrá resolver")
 
     pila_nodos = [] # nombres de los nodos (array de strings)
-
 
 
     resultado = {}
@@ -157,17 +109,15 @@ def main():
             solucion = False
             variables_no_enteras.append(v.name)
             
-    # print("Diccionario resultado: ", resultado)
-    # print("Lista de variables no enteras: ", variables_no_enteras)
     
     if solucion == True:
         print('El problema es automaticamente entero, ya no es necesario realizar árbol de decisión.')
         mejor_nodo['nodo'] = 'original'
         mejor_nodo['z'] = value(problema.objective)
         mejor_nodo['resultado'] = resultado
-        print("\nSolución optima:")
-        print("Z*=", resultado["obj"])
-        print("X*=(",resultado["Xs_x1"],resultado["Xs_x2"],")")
+        print("\n Solución optima: ")
+        print("Z* = ", resultado["obj"])
+        print("X* = (",resultado["Xs_x1"],resultado["Xs_x2"],")")
     else: # Agregamos el root a la lista de nodos
         print("Se realizará árbol de decisión, ya que no es entero")
         pila_nodos.append('original')
@@ -201,15 +151,11 @@ def main():
         variables_no_enteras = arbol.get_node('{nodo}'.format(nodo=nodo_ramificar)).data['no_enteros']
 
         variable_ramificar = random.choice(variables_no_enteras)
-        # print("Variable por la que se va a ramificar: ", variable_ramificar)
 
         # Aqui 3.5 pasa a 3
         piso = int(arbol.get_node('{nodo}'.format(nodo=nodo_ramificar)).data['resultado'][variable_ramificar])
 
         techo = math.ceil(arbol.get_node('{nodo}'.format(nodo=nodo_ramificar)).data['resultado'][variable_ramificar])
-
-        # print("Piso: ", piso)
-        # print("Techo: ", techo)
 
         # Copiar el original, porque le vamos a añadir una restriccion
         nodo = arbol.get_node('{nodo}'.format(nodo=nodo_ramificar)).data['problema'].copy()
@@ -223,10 +169,6 @@ def main():
         # print(nodo) # subproblema 1
 
         nodo.solve()
-
-        # for variable in nodo.variables():
-                # print(variable.name, "=", variable.varValue)
-        # print(value(nodo.objective)) # z* del subproblema 1
 
         # Inicializar resultado
         resultado = {}
@@ -252,23 +194,15 @@ def main():
                         pila_nodos.append('node{no}'.format(no = p))
                         # print("Pila nodos: ", pila_nodos)
                     else: 
-                        if tipo == 'LpMaximize':
-                            if resultado['obj'] > mejor_nodo['z']:
-                                pila_nodos.append('node{no}'.format(no=p))
-                        else:
-                            if resultado['obj'] < mejor_nodo['z']:
-                                pila_nodos.append('node{no}'.format(no=p))
+                        if resultado['obj'] > mejor_nodo['z']:
+                            pila_nodos.append('node{no}'.format(no=p))
+                        
             else: # todas las variables son enteras (una hoja)
-                if tipo == 'LpMaximize':
-                    if resultado['obj'] > mejor_nodo['z']:
+                if resultado['obj'] > mejor_nodo['z']:
                         mejor_nodo['node'] = 'node{no}'.format(no=p)
                         mejor_nodo['z'] = resultado['obj']
                         mejor_nodo['resultado'] = resultado
-                else:
-                    if resultado['obj'] < mejor_nodo['z']:
-                        mejor_nodo['node'] = 'node{no}'.format(no=p)
-                        mejor_nodo['z'] = resultado['obj']
-                        mejor_nodo['resultado'] = resultado
+                    
             
             arbol.create_node('node{no}'.format(no=p), 'node{no}'.format(no=p), 
                             parent='{node}'.format(node=nodo_ramificar),
@@ -328,23 +262,14 @@ def main():
                         pila_nodos.append('node{no}'.format(no = p))
                         # print("Pila nodos: ", pila_nodos)
                     else: 
-                        if tipo == 'LpMaximize':
-                            if resultado['obj'] > mejor_nodo['z']:
-                                pila_nodos.append('node{no}'.format(no=p))
-                        else:
-                            if resultado['obj'] < mejor_nodo['z']:
-                                pila_nodos.append('node{no}'.format(no=p))
+                        if resultado['obj'] > mejor_nodo['z']:
+                                pila_nodos.append('node{no}'.format(no=p))    
             else: # todas las variables son enteras (una hoja)
-                if tipo == 'LpMaximize':
-                    if resultado['obj'] > mejor_nodo['z']:
+                if resultado['obj'] > mejor_nodo['z']:
                         mejor_nodo['node'] = 'node{no}'.format(no=p)
                         mejor_nodo['z'] = resultado['obj']
                         mejor_nodo['resultado'] = resultado
-                else:
-                    if resultado['obj'] < mejor_nodo['z']:
-                        mejor_nodo['node'] = 'node{no}'.format(no=p)
-                        mejor_nodo['z'] = resultado['obj']
-                        mejor_nodo['resultado'] = resultado
+                    
             
             arbol.create_node('node{no}'.format(no=p), 'node{no}'.format(no=p), 
                             parent='{node}'.format(node=nodo_ramificar),
@@ -371,10 +296,56 @@ def main():
     # FIN DEL CICLO WHILE
 
 
+def resultado():
+    o = 0
+
+    nodos_ordenados_indices = [0] # el original es 0
+    nodos_ordenados = []
+
+    for nombre in list(arbol.expand_tree(mode=Tree.DEPTH))[1:]:
+         nodos_ordenados_indices.append(int(nombre[4]))
+
+
+    for i in nodos_ordenados_indices:
+
+        for j, nodo in enumerate(arbol.all_nodes()):
+            if i == j:
+                nodos_ordenados.append(nodo)
+    
+    # print("Indices  ordenados: ", nodos_ordenados_indices)
+
+    for i, nodo in enumerate(nodos_ordenados):
+        if i == 0:
+            print("\n\n~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~")
+            print("\n\n\t Problema original \n\n")
+            print("~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~")
+        else:
+            print("\n\n~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~")
+            print("\n\n\t Subproblema ", i, " \n\n")
+            print("~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~")
+        print(nodo)
+        print("~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~~o~")
+
+    for o,nodo in enumerate(nodos_ordenados):
+        if nodo.data['resultado']['obj'] == mejor_nodo['z']:
+            num=o
+    # print("Nodos ordenados: ", nodos_ordenados)
+
+    print("\n\n----------------------------------")
+    print("\n\n\t Solucion óptima: \n")
+    print("\n\t Subproblema ", num,)
+    print("----------------------------------")
+    print("\n z* = ", int(mejor_nodo['z']))
+
+    for i, variable in enumerate(mejor_nodo['resultado']['vars'].keys()):
+        print("\n v_x{num} = ".format(num=i+1), int(mejor_nodo['resultado'][variable]))
+
+    print("----------------------------------")    
 
 
 if __name__ == '__main__':
-    main()
+    input()
+    proceso()
     resultado()
     
 
